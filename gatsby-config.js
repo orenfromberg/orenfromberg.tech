77c2591871b2705dfebdd4b51a01059b7958a72b
Oren Fromberg <oren@adhocteam.us>
2019-11-01 09:19:02 -0400

    Updating icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,7 +133,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/blog-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
