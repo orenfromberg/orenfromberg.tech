5d7b74d114d263c5eef83b3c561b1d5835de718e
Oren Fromberg <oren@adhocteam.us>
2023-08-14 11:40:35 -0400

    Updating typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -15,8 +15,16 @@ import Typography from "typography"
 
 // import moragaTheme from "typography-theme-moraga"
 // const typography = new Typography(moragaTheme)
-import irvingTheme from "typography-theme-irving"
-const typography = new Typography(irvingTheme)
+// import irvingTheme from "typography-theme-irving"
+// const typography = new Typography(irvingTheme)
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.66,
+  headerFontFamily: ['Verdana','Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Georgia', 'serif'],
+  // See below for the full list of options.
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
