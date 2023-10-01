667b062fc186650f1d726961d91793579e252882
Oren Fromberg <orenfromberg@gmail.com>
2023-10-01 13:45:56 -0400

    Adding katex

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,13 @@ module.exports = {
         plugins: [
           `gatsby-remark-prismjs-copy-button`,
           {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
